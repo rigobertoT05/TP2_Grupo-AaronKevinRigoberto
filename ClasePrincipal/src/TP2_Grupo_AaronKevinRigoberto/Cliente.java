2c3575daef433a3ea0cd4810ac8025bba37bec7c
rigoberto <151102878+rigobertoT05@users.noreply.github.com>
2025-11-22 21:45:52 -0600

    Implementacion de clases

---
+++ ClasePrincipal/src/TP2_Grupo_AaronKevinRigoberto/Cliente.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP2_Grupo_AaronKevinRigoberto;
+
+/**
+ *
+ * @author rigoberto
+ */
+public class Cliente {
+    private int identificadorUnico;
+    private String nombre;
+    private int numeroTelefono;
+    private String correoElectronico;
+
+    public Cliente(int identificadorUnico, String nombre, int numeroTelefono, String correoElectronico) {
+        this.identificadorUnico = identificadorUnico;
+        this.nombre = nombre;
+        this.numeroTelefono = numeroTelefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public int getIdentificadorUnico() {
+        return identificadorUnico;
+    }
+
+    public void setIdentificadorUnico(int identificadorUnico) {
+        this.identificadorUnico = identificadorUnico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(int numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
+    
+}
