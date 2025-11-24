162d598615bcbab4e63b0024efc5de363f2a24b5
Kevin Rugama17 <kevinrugama171@gmail.com>
2025-11-24 12:11:24 -0600

    Creacion clase gestor clientes

---
+++ ClasePrincipal/src/TP2_Grupo_AaronKevinRigoberto/GestorCliente.java
@@ -0,0 +1,12 @@
+package TP2_Grupo_AaronKevinRigoberto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kevin
+ */
+public class GestorCliente {
+      public static ArrayList<Cliente> listaClientes = new ArrayList<>();
+    
+}
